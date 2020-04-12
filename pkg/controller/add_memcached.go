143cb0c63391acb339cb695328aa1adc7002cbcb
Dmitry Korotin <dev.korotin@gmail.com>
2020-04-12 20:55:40 +0400

    generate controller

---
+++ pkg/controller/add_memcached.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/soxat/operator-sdk-testing/pkg/controller/memcached"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, memcached.Add)
+}
